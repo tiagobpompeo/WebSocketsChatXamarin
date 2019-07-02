6035d7bac2ebb539fe7f15e5aa9e4c58fe2b58ee
Tiago Pompeo <tiapom@gmail.com>
2019-07-01 22:33:56 -0300

    Initial commit

---
+++ MyChat/MyChat/CelulasCustomizadas/OutgoingViewCell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MyChat.CelulasCustomizadas
+{
+    public partial class OutgoingViewCell : ViewCell
+    {
+        public OutgoingViewCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MyChat/MyChat/MyDataTemplateSelector.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using MyChat.CelulasCustomizadas;
+using Plugin.DeviceInfo;
+using Xamarin.Forms;
+
+namespace MyChat
+{
+    public class MyDataTemplateSelector : Xamarin.Forms.DataTemplateSelector
+    {
+        public MyDataTemplateSelector()
+        {
+            // Retain instances!
+            this.incomingDataTemplate = new DataTemplate(typeof(IncomingViewCell));
+            this.outgoingDataTemplate = new DataTemplate(typeof(OutgoingViewCell));
+        }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var messageVm = item as Message;
+            if (messageVm == null)
+                return null;
+            return messageVm.IsIncoming ? this.incomingDataTemplate : this.outgoingDataTemplate;
+        }
+
+        private readonly DataTemplate incomingDataTemplate;
+        private readonly DataTemplate outgoingDataTemplate;
+    }
+
+
+    public class Message
+    {
+        public string Text { get; set; }
+        public DateTime MessagDateTime { get; set; }
+
+        public bool IsIncoming => UserId != CrossDeviceInfo.Current.Id;
+
+        public string Name { get; set; }
+        public string UserId { get; set; }
+    }
+}
